c9e6a7ac262af11beaa896c847e748beeffba849
Mo Li <mol@fb.com>
2021-01-03 15:52:47 -0800

    add weather station project

---
+++ Weather Data/Weather Data/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Weather Data
+//
+//  Created by Mo Li on 1/3/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
